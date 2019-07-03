191cd24caeddaa479018a419e4874508bcfad77e
devang <devangvachheta123@gmail.com>
2019-07-03 13:58:37 +0530

    WP Plugin structure
    Start Wordpress Plugin Development

---
+++ rt-slideshow.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ * 
+
+    * @wordpress-plugin
+ * Plugin Name:       rtCamp Slideshow Plugin Assignment
+ * Plugin URI:        https://github.com/devang137/rtcamp-plugin-assignment-1-master.zip
+ * Description:       This is a short description of what the plugin does.
+ * Version:           1.0.0
+ * Author:            Devang Vachheta
+ * Author URI:        https://github.com/devang137
+ * License:           GPL2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+defined('ABSPATH') || die("Hey, You can't access this file!");
+
+
---
+++ uninstall.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * 
+ * @link       
+ * @since      1.0.0
+ *
+ */
+
+
+// If uninstall not called from WordPress, then exit
+    if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+        exit;
+    }
